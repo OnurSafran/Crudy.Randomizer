2c259cabfe0d108c8fa8f0848b8efd5806780954
OnurSafran <onur.safran@hotmail.com>
2022-10-06 10:27:56 +0300

    Builder revised

--- src/Crudy.Randomizer/RandomizerBuilder.cs
+++ src/Crudy.Randomizer/RandomizerBuilder.cs
@@ -1,27 +1,42 @@
 ﻿namespace Crudy.Randomizer; 
 
 public class RandomizerBuilder<T> where T : notnull {
-	private readonly Randomizer<T> _randomizer;
+	private RandomizerConfiguration? _randomizerConfiguration = null;
+	private Dictionary<T, int>? _dictionary = null;
+	private List<T>? _excludedKeys = null;
 
 	public RandomizerBuilder() {
-		_randomizer = new Randomizer<T>();
 	}
 	
 	public RandomizerBuilder(RandomizerConfiguration randomizerConfiguration) {
-		_randomizer = new Randomizer<T>(randomizerConfiguration);
+		_randomizerConfiguration = randomizerConfiguration;
+	}
+
+	public RandomizerBuilder<T> Configuration(RandomizerConfiguration randomizerConfiguration) {
+		_randomizerConfiguration = randomizerConfiguration;
+		return this;
 	}
 
 	public RandomizerBuilder<T> Data(Dictionary<T, int> dictionary) {
-		_randomizer.Add(dictionary);
+		_dictionary = dictionary;
 		return this;
 	}
 
 	public RandomizerBuilder<T> Exclude(List<T> excludeList) {
-		_randomizer.AddExcludes(excludeList);
+		_excludedKeys = excludeList;
 		return this;
 	}
 
-	public Randomizer<T> Build() {
-		return _randomizer;
+	public Randomizer<T> Build()
+	{
+		var randomizer = _randomizerConfiguration != null ? new Randomizer<T>(_randomizerConfiguration) : new Randomizer<T>();
+		
+		if(_dictionary != null)
+			randomizer.Add(_dictionary);
+		
+		if(_excludedKeys != null)
+			randomizer.AddExcludes(_excludedKeys);
+		
+		return randomizer;
 	}
 }
